24f7db70bf9769eb6d4fcafcfce6b77882eef9ec
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-11-09 12:05:20 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FintechHackathon13::Application.config.secret_key_base = '9dfee377ec93fd12d792378895d7be8add4728434aac5eb3579fa8c5feb6fa91779a47b0e23c6da671649d70cd5cc92c15efee0090f4522af076f4bd696bdbd0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FintechHackathon13::Application.config.session_store :cookie_store, key: '_fintech-hackathon-13_session'
